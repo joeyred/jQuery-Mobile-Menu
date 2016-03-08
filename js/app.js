ec175ce6c7792da0cf0c6e75cbf804beedfc9abf
Joey Hayes <bjoeyhayes@gmail.com>
2016-03-08 15:33:43 -0500

    JS preparation

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Problem: Menu breaks on smaller screens
+// Solution: To hide the text links and swap them out for a more appropriate navigation
+
+// Create a select element and append to menu. Options should use text links to get their info.
+// Cycle over menu links
+	// options value is href
+	// options text is the text of the link
+	// append option to select
+// create button to click to go to select's location
+// Bind Click to button
+	// Go to select's location
+// Modify CSS to hide links on small width and show button and select
+	// also hides select and button on larger width and show's links
