b3f04638fd2f6a09cad30490da0150ef09df5955
Joey Hayes <bjoeyhayes@gmail.com>
2016-03-09 16:44:53 -0500

    select element added and options populated

--- js/app.js
+++ js/app.js
@@ -2,10 +2,22 @@
 // Solution: To hide the text links and swap them out for a more appropriate navigation
 
 // Create a select element and append to menu. Options should use text links to get their info.
+var $select = $( '<select></select>' );
+$( '#menu' ).append( $select );
+
 // Cycle over menu links
+$( '#menu a' ).each(function() {
+	var $anchor = $( this );
+	var $option = $( '<option></option>' );
+
 	// options value is href
+	$option.val( $anchor.attr('href') );
 	// options text is the text of the link
+	$option.text( $anchor.text() );
 	// append option to select
+	$select.append( $option );
+});
+	
 // create button to click to go to select's location
 // Bind Click to button
 	// Go to select's location
